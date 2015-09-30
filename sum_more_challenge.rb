4096f5998361929f2a45595d027abdc9802d3987
Speaslee <sophiapeaslee@gmail.com>
2015-09-30 09:42:48 -0400

    sums

--- sum_more_challenge.rb
+++ sum_more_challenge.rb
@@ -4,9 +4,25 @@ require 'pry'
 
 numbers=[]
 File.foreach("numbers.csv", converters: :all) do |row|
-  row.split
-  numbers << row.to_i
+  row.split("\n")
+  numbers << row
 end
+first_line=0
+line_first = numbers.first.split ","
+line_first.each do |num|
 
-sum_all = numbers
-puts sum_all.reduce(:+)
+  first_line = first_line + num.to_i
+end
+puts first_line
+
+
+counter = 0
+nums= 0
+numbers.each do |line|
+all_nums = line.split ","
+all_nums.each do |total|
+  nums= nums + total.to_i
+  counter= counter +1
+end
+end
+puts nums
